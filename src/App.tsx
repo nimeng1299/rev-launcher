1a9858fd931f943acb6de6774cefc5620cc6fff6
lumine <463305152@qq.com>
2025-05-11 01:02:56 +0800

    change titleBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import styles from "./TitleBar.module.css";
+import { Text, ToolbarButton } from "@fluentui/react-components";
+import {
+  ArrowMinimizeRegular,
+  DismissRegular,
+  ListRegular,
+  SquareRegular,
+} from "@fluentui/react-icons";
+import { Window } from "@tauri-apps/api/window";
 
 function App() {
+  const appWindow = new Window("main");
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [isMaximized, setIsMaximized] = useState(false);
+
+  const handleMinimize = () => appWindow.minimize();
+  const handleToggleMaximize = async () => {
+    const newState = !isMaximized;
+    await appWindow.toggleMaximize();
+    setIsMaximized(newState);
+  };
+  const handleClose = () => appWindow.close();
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -14,19 +32,34 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div data-tauri-drag-region className={styles.titleBar}>
+        <div className={styles.leftSection}>
+          <ToolbarButton
+            className={styles.toolbarButton}
+            icon={<ListRegular />}
+          />
+          <Text>Rev Launcher</Text>
+        </div>
+        <div data-tauri-drag-region />
+        <div>
+          <ToolbarButton
+            aria-label="Minimize"
+            icon={<ArrowMinimizeRegular />}
+            onClick={handleMinimize}
+          />
+          <ToolbarButton
+            aria-label={isMaximized ? "Restore" : "Maximize"}
+            icon={isMaximized ? <SquareRegular /> : <SquareRegular />}
+            onClick={handleToggleMaximize}
+          />
+          <ToolbarButton
+            aria-label="Close"
+            icon={<DismissRegular />}
+            onClick={handleClose}
+          />
+        </div>
       </div>
+      <h1>Welcome to Tauri + React</h1>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
