d9fe538846bc869e092dd6ae46d7dbb92268ee89
lumine <463305152@qq.com>
2025-06-05 22:45:04 +0800

    add titleBar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { Toaster } from "@skeletonlabs/skeleton-svelte";
+    import { toaster } from "./toaster-svelte";
+    import "../app.css";
+
+    let { children } = $props();
+</script>
+<Toaster {toaster}></Toaster>
+{@render children()}
