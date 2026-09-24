07f8e79b00b6597b8f51dc245832143a6910678d
lumine <463305152@qq.com>
2026-09-24 11:34:04 +0800

    launcher: fix account data save

--- launcher/src/data/account_data.rs
+++ launcher/src/data/account_data.rs
@@ -4,7 +4,7 @@ use mclib::account::Account;
 use serde::{Deserialize, Serialize};
 use std::ops::{Deref, DerefMut};
 
-const ACCOUNT_DATA_FILE: &str = "settings.json";
+const ACCOUNT_DATA_FILE: &str = "account.json";
 
 #[derive(Default, Deserialize, Serialize)]
 pub struct AccountData {
@@ -30,7 +30,7 @@ impl AccountData {
     pub fn save(&self) -> anyhow::Result<()> {
         let path = dirs::config_dir().context("Failed to read current directory")?;
         let pick_path = path.join("rev_launcher");
-        std::fs::create_dir_all(pick_path.clone())
+        std::fs::create_dir_all(&pick_path)
             .context("Failed to create settings directory")?;
         let filename = pick_path.join(ACCOUNT_DATA_FILE);
         let data = serde_json::to_string_pretty(self).context("Failed to serialize settings")?;
